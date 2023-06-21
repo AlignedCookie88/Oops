a03a31bccbe8734afc99984004a837aa31da75b8
AlignedCookie88 <52969840+AlignedCookie88@users.noreply.github.com>
2023-06-21 10:36:08 +0100

    Update installpckgs.py

--- installpckgs.py
+++ installpckgs.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("pip install --trusted-host pypi.org playsound config")
-os.system("pip install --trusted-host pypi.org pynput config")
+os.system("pip install --trusted-host pypi.org playsound")
+os.system("pip install --trusted-host pypi.org pynput")
